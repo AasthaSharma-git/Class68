4fa07378f64a01c942c5c6bbb1f33fb0748d51a7
AasthaSharma-git <67312804+AasthaSharma-git@users.noreply.github.com>
2021-09-11 13:46:48 +0530

    Add files via upload

---
+++ screens/BookTransaction.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+class BookTransaction extends React.Component{
+  render(){
+    return(
+       <View style={{alignItems:'center'}}>
+       <Text>Issue or Return</Text>
+       </View>
+
+    );
+
+  }
+}
+
+export default BookTransaction;
